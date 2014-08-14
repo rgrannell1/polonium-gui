7eccbb78a1a3c8f83b115f5c914b7383d9d55df8
rgrannell1 <r.grannell2@gmail.com>
2014-08-14 17:50:30 +0100

    continued work on executable

--- execute.sh
+++ execute.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env sh
 
-$dir="~/polonium-gui"
-
-if [ ! -d "$dir" ]
+if [ ! -d ~/polonium-gui/node-webkit-v0.8.6-linux-x64 ]
 then
-	wget "http://dl.node-webkit.org/v0.8.6/node-webkit-v0.8.6-linux-x64.tar.gz" -P $dir
-	tar xvzf "node-webkit-v0.8.6-linux-x64.tar.gz"
+
+	echo '-- polonium-gui: downloading node webkit.'
+
+	wget -O ~/polonium-gui/webkit.tar.gz http://dl.node-webkit.org/v0.8.6/node-webkit-v0.8.6-linux-x64.tar.gz
+	cd ~/polonium-gui/
+
+	tar xvzf webkit.tar.gz
+
 else
-	echo hi
+
+	echo '--polonium-gui: executing polonium-gui.'
+
+	# -- use nw to execute app.zip
+	~/polonium-gui/node-webkit-v0.8.6-linux-x64/nw ~/polonium-gui/app.zip
+
 fi
