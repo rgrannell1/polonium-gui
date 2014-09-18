55de360058172b48a21cdf50b94b2ae79cd98f3b
rgrannell1 <r.grannell2@gmail.com>
2014-09-18 11:40:29 +0100

    working on time logging

--- execute.sh
+++ execute.sh
@@ -13,8 +13,14 @@ then
 else
 
 	echo '--polonium-gui: executing polonium-gui.'
+	data_path=~/polonium-gui/test/data/load_times.txt
+
+	# echo the start time to a file
+	echo $(($(date +%s%N)/1000000)) >> $data_path
+	echo "\n" >> $data_path
 
 	# -- use nw to execute app.zip
+	# -- this will write the final startup time to a file on load.
 	~/polonium-gui/node-webkit-v0.8.6-linux-x64/nw ~/polonium-gui/app.zip
 
 fi
